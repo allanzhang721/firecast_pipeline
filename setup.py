9f1736f50920e0224c548b14cc0d452134131850
allanzhang721 <123775367+allanzhang721@users.noreply.github.com>
2025-05-31 17:05:15 -0400

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="firecast_pipeline",
-    version="0.1.0",
+    version="0.1.1",
     description="Unified regression pipeline for fire risk prediction.",
     author="Allan Zhang",
     author_email="your.email@example.com",
